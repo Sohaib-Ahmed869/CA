1e7b2b032bb7ca254d40551e2414dd64cb7dec68
asadawan16 <asadawan16900@gmail.com>
2025-03-18 20:44:50 +0500

    ok

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,17 +6,15 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDCfrf4npemD5T0ZzPcfRv3Kiq--Vs3m1A",
-  authDomain: "certifiedaustralia1.firebaseapp.com",
-  projectId: "certifiedaustralia1",
-  storageBucket: "certifiedaustralia1.appspot.com",
-  messagingSenderId: "803606042865",
-  appId: "1:803606042865:web:e80a85af2ec46a9aa43150",
-  measurementId: "G-ESBEFL4ZJ9",
+  apiKey: "AIzaSyAlDj_YjopzasGvveP1VNxJsMOk-fBICO8",
+  authDomain: "testca-e3e5e.firebaseapp.com",
+  projectId: "testca-e3e5e",
+  storageBucket: "testca-e3e5e.firebasestorage.app",
+  messagingSenderId: "92801329690",
+  appId: "1:92801329690:web:6daa5db44133212b3bffa3",
+  measurementId: "G-V2Z5CZHSPQ",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
